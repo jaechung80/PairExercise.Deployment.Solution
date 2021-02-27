313c9f1fe4eb0da086414fba353948e6ca8b695c
Jae Chung <jaepublic@gmail.com>
2021-02-26 21:55:16 -0600

    chore: add dialect to sequelize config

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
-const dbUrl =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}?ssl=true`;
+const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`;
 let config;
+
 if (process.env.DATABASE_URL) {
   config = {
     logging: false,
+    dialect: 'postgres',
     ssl: true,
     dialectOptions: {
       ssl: {
